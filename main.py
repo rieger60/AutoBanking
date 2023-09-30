b8d380413790fb3f1e3e7f823771aa5e10308388
rieger60 <57003243+rieger60@users.noreply.github.com>
2023-09-30 11:25:43 +0200

    Update main.py
    Created extract CSV data for two banks, Wise and Danske Bank

--- main.py
+++ main.py
@@ -1,19 +1,86 @@
+# Import libraries
+import pandas as pd
+
 # Import the categorization_rules from an external module
 from categorization_rules import categorization_rules
 
-# Function to extract data from a CSV file
-def extract_csv_data(file_path):
-    # Implement this function to extract data from a CSV file
-    pass
+def extract_csv_data(file_path, bank):
+    try:
+        if bank == 'Danske Bank':
+            # Specific settings for Danske Bank
+            delimiter = ';'
+            encoding = 'ISO-8859-1'
+
+            # Read the CSV file into a pandas DataFrame with Danske Bank-specific settings
+            df = pd.read_csv(file_path, delimiter=delimiter, encoding=encoding)
+
+            # Rename columns for consistency and readability for Danske Bank
+            df = df.rename(columns={'Dato': 'Date', 'Tekst': 'Description', 'Beløb': 'Amount'})
+
+            # Format the 'Date' column to 'DD-MM-YYYY' format for Danske Bank
+            df['Date'] = pd.to_datetime(df['Date'], format='%d.%m.%Y').dt.strftime('%d-%m-%Y')
+
+            # Remove thousand separator (dot) and replace comma with dot for decimal delimiter in 'Amount' column for Danske Bank
+            df['Amount'] = df['Amount'].str.replace('.', '').str.replace(',', '.', regex=False).astype(float)
+
+        elif bank == 'Wise':
+            # Specific settings for Wise bank (add more banks as needed)
+            delimiter = ','  # Change this delimiter as per the CSV format
+            encoding = 'utf-8'  # Change encoding if necessary
+
+            # Read the CSV file into a pandas DataFrame with settings for Wise bank
+            df = pd.read_csv(file_path, delimiter=delimiter, encoding=encoding)
+
+            # Filter out unwanted rows
+            df = df[(df['Status'] != 'CANCELLED') & (df['Target amount (after fees)'] != 0) & (df['Source amount (after fees)'] != 0)]
+
+            # Format the date column to 'DD-MM-YYYY' format
+            df['Date'] = pd.to_datetime(df['Finished on'], format='%Y-%m-%d %H:%M:%S').dt.strftime('%d-%m-%Y')
+
+            # Initialize empty columns
+            df['Description'] = ''
+            df['Amount'] = ''
+            df['Currency'] = ''
+
+            # Set values based on 'Direction' column
+            df.loc[df['Direction'] == 'IN', 'Description'] = df['Source name']
+            df.loc[df['Direction'] == 'OUT', 'Description'] = df['Target name']
+            df.loc[df['Direction'] == 'NEUTRAL', 'Description'] = 'Internal transfer'
+
+            df.loc[df['Direction'] == 'IN', 'Amount'] = df['Source amount (after fees)']
+            df.loc[df['Direction'] == 'OUT', 'Amount'] = df['Target amount (after fees)'] * -1
+            df.loc[df['Direction'] == 'NEUTRAL', 'Amount'] = 0
+
+            df.loc[df['Direction'] == 'IN', 'Currency'] = df['Source currency']
+            df.loc[df['Direction'] == 'OUT', 'Currency'] = df['Target currency']
+            df.loc[df['Direction'] == 'NEUTRAL', 'Currency'] = df['Target currency']
+
+        else:
+            raise ValueError("Unsupported bank")
+
+        # Convert the DataFrame to a list of dictionaries
+        transactions = df.to_dict(orient='records')
+
+        # Print an example of the DataFrame
+        print("Example DataFrame:")
+        print(df[['Date', 'Description', 'Amount', 'Currency']].head())  # This prints the first 5 rows of the DataFrame
+
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+        transactions = []
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        transactions = []
+
+    return transactions
 
 # Function to extract data from a PDF file using Tabula
-def extract_pdf_data(file_path):
+def extract_pdf_data(file_path, bank):
     # Implement this function to extract data from a PDF file using Tabula
     pass
 
-# Function to extract data from an XML file
-def extract_xml_data(file_path):
-    # Implement this function to extract data from an XML file
+def extract_xml_data(file_path, bank):
+    # Implement this function to extract data from a PDF file using Tabula
     pass
 
 # Function to categorize transactions based on rules
@@ -37,25 +104,27 @@ def find_uncategorized_transactions(transactions, categorized_transactions):
 # Main function
 def main():
     # Specify the path to the statement file (CSV, PDF, or XML)
-    file_path = 'path/to/your/statement.xml'  # Update with your file path
+    file_path = 'sample.csv'  # Update with your file path
+    bank = 'Wise'
     
     # Extract data from the statement file based on its format
     if file_path.endswith('.csv'):
-        transactions = extract_csv_data(file_path)
+        transactions = extract_csv_data(file_path, bank)
     elif file_path.endswith('.pdf'):
-        transactions = extract_pdf_data(file_path)
+        transactions = extract_pdf_data(file_path, bank)
     elif file_path.endswith('.xml'):
-        transactions = extract_xml_data(file_path)  # Use the new XML function
+        transactions = extract_xml_data(file_path, bank)
     else:
         raise ValueError("Unsupported file format")
+
     
     # Use categorization_rules from the external module
     # Define categorization rules (imported from categorization_rules.py)
-    categorization_rules = categorization_rules
+    categorization_rules_list = categorization_rules
     
     # Categorize transactions
-    categorized_transactions = categorize_transactions(transactions, categorization_rules)
-
+    categorized_transactions = categorize_transactions(transactions, categorization_rules_list)
+    
     # Find and collect uncategorized transactions
     uncategorized_transactions = find_uncategorized_transactions(transactions, categorized_transactions)
     
